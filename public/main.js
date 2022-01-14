65dd50787a279513fdae0d486765b3c952f5282d
Chun Hong Terrence Ng <s10194044@connect.np.edu.sg>
2022-01-14 14:40:01 +0800

    commit on 'master'

--- public/main.js
+++ public/main.js
@@ -1,26 +1,38 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
+///// User Authentication /////
+
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
+
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+
+const userDetails = document.getElementById('userDetails');
+
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+/// Sign in event handlers
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // signed in
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
     } else {
-      x.className = "topnav";
+        // not signed in
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = '';
     }
-  }
-  
-document.addEventListener("DOMContentLoaded", event => {
-    const app = firebase.app()
-    console.log(app)
-
-    const db = firebase.firestore();
-
-    const xaver = db.collection('users').doc('eygfOkBh85QeQ6ytk2lp');
-
-    xaver.get()
-            .then(doc => {
-                const data = doc.data();
-                document.write(data.name+ '<br>')
-                document.write(data.occupation+ '<br>')
-                document.write(data.compoundScore + '<br>')
-            })
-
 });
+
+$('.carousel').carousel({
+  interval: 10000
+})
